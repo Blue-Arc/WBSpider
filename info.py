c9fe2b19cc2a628ae8f47e13a67df3cef8ce0c94
Blue-Arc <1321584076@qq.com>
2022-04-26 00:37:10 +0800

    :dog: 添加cookie校验和

--- info.py
+++ info.py
@@ -1,9 +1,20 @@
 import json
+
+from time import sleep
 from utils import Spider
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 class Info(object):
     def __init__(self, cookie):
         self.spider = Spider(Cookie=cookie)
+        
+    def cookie_validate(self):
+        url = "https://weibo.com"
+        res = self.spider.get(url)
+        if res.status_code == "302":
+            return True
+        else:
+            return False
         
     def get_Info(self, name:str):
         """获取用户输入的微博名，搜索相关人物供选择
@@ -119,7 +130,7 @@ class Info(object):
             uid (str): 用户uid
         """
         url = f"https://weibo.com/ajax/statuses/mymblog?uid={uid}&page=1&feature=0"
-        res = self.spider.get(url)
+        # res = self.spider.get(url)
         # with open('statuses.json', 'w+', encoding='utf-8') as f:
         #     json.dump(json.loads(res.text),f)
         #     f.close() 
@@ -150,6 +161,7 @@ class Info(object):
             num (str): 动态序号
         """
         sid = dic['data']['list'][int(num)-1]['id']
+        print("#"*50 + num + "#"*50)
         print("发帖内容:%s" % dic['data']['list'][int(num)-1]['text_raw'])
         url = f"https://weibo.com/ajax/statuses/buildComments?is_reload=1&id={sid}&is_show_bulletin=2&is_mix=0&count=10&uid={uid}"
         # with open('comments.json', 'w+', encoding='utf-8') as f:
@@ -166,9 +178,61 @@ class Info(object):
             print("评论内容:%s" % info_dict['text'])
             print("点赞数:%s" % info_dict['like_counts'])
             i += 1  
-                      
+            
+    def get_AllComments(self, uid:str):
+        """获取所有动态及其评论
+
+        Args:
+            uid (str): 用户uid
+        """
+        url = f"https://weibo.com/ajax/statuses/mymblog?uid={uid}&page=1&feature=0"
+        json_dict = self.spider.get_json(url)
+        i = 1
+        with ThreadPoolExecutor(max_workers = 10) as pool: # with创建线程池会阻塞到所有线程任务完成
+            futures = []
+            for info_dict in json_dict['data']['list']:
+                if i>10:break
+                print('*'*50 + str(i) + '*'*50)
+                retweeted_status = False
+                if 'retweeted_status' in info_dict:
+                    retweeted_status = True               
+                print("发帖时间:%s" % info_dict['created_at'])
+                print("发帖内容:%s" % info_dict['text_raw'])
+                print("转发数:%s" % info_dict['reposts_count'])
+                print("评论数:%s" % info_dict['comments_count'])
+                print("点赞数:%s" % info_dict['attitudes_count'])
+                print("是否为转发:%s" % retweeted_status)
+                if retweeted_status:
+                    print("转发内容:%s" % info_dict['retweeted_status']['text_raw'])
+                futures.append(pool.submit(self.get_Comments, i, uid, json_dict))       
+                i += 1 
+            as_completed(futures)
+            
+    def start_surpervise(self, uid:str):
+        """实时监控动态的更新变化
+
+        Args:
+            uid (str): 用户uid
+        """
+        url = f"https://weibo.com/ajax/statuses/mymblog?uid={uid}&page=1&feature=0"
+        json_dict = self.spider.get_json(url)
+        while True:
+            # 更新id值
+            flag = False
+            if("isTop" in json_dict["data"]["list"][0]): # 置顶动态更新后不变
+                id = json_dict["data"]["list"][1]['id']
+            else:
+                id = json_dict["data"]["list"][0]['id']
+            sleep(30)
+            json_dict = self.spider.get_json(url)
+            if (id != json_dict["data"]["list"][0]['id']):
+                flag = True
+            if flag: print("[+]目标动态已更新, 请及时查看")
+                    
+             
 if __name__ == "__main__":
     with open('cookies.json','r') as f:
         cookie = json.load(f)
     info = Info(cookie=cookie)
-    print(info.get_Info("赵今麦"))
+    # print(info.get_Info("赵今麦"))
+    info.get_Statuses("7395299383")
